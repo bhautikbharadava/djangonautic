d0b19f0d55af1ecded989d7a2b213ae3545ff6d7
Bhautik Bharadava <bhautikbharadava@gmail.com>
2018-07-30 18:30:14 +0530

    Html rendering

--- djangonautic/views.py
+++ djangonautic/views.py
@@ -1,8 +1,11 @@
+
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Home page")
+    # return HttpResponse("Home page")
+    return render(request,'homepage.html')
 
 def about(request):
-    return HttpResponse("About")
-
+    # return HttpResponse("About")
+    return render(request,'about.html')
